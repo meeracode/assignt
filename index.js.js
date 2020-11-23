3fc0301eb01f0a049d4519e8fc7fa26a426e9752
meeracode <71360148+meeracode@users.noreply.github.com>
2020-11-22 21:08:30 -0800

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,43 @@
+const prof = require("./profile");
+
+// class profiles extends Vehicle {
+//   constructor(id, type, crew) {
+//     super(id, 0, "bwom");
+//     this.type = type;
+//     this.crew = crew;
+//   }
+//   useHorn() {
+//     console.log(this.sound);
+//   }
+//   crewSoundOff() {
+//     this.crew.forEach(member => {
+//       console.log(`${member.name} reporting for duty!`);
+//     });
+//   }
+// }
+
+// const boatPassengers = [
+//   {
+//     name: "Blackbeard"
+//   },
+//   {
+//     name: "Mary Read"
+//   },
+//   {
+//     name: "Henry Morgan"
+//   },
+//   {
+//     name: "Madame Cheng"
+//   }
+// ];
+
+
+  // id=1,
+  // profession = "jobs", name = "John doe",
+  // numberOfyears = 29, website = "https://death.com");
+
+console.log("---DAED---");
+pofs = new prof(id = 1,
+  profession = "jobs", name = "John doe",
+  numberOfyears = 29, website = "https://death.com");
+pofs.printInfo();
---
+++ profile.js
@@ -0,0 +1,15 @@
+class profile {
+  constructor(id, profession = "death", name = "John doe", numberOfyears= 0, website = "https://death.com") {
+    this.id = id;
+    this.name = name;
+    this.profession = profession;
+    this.work = 0;
+    this.website = website;
+  }
+
+  printInfo() {
+    console.log(`Number of years ${this.name} website development`);
+    console.log(`Number of years ${this.name} website development`);
+  }
+}
+module.exports = profile;
