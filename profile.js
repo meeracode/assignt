395bc28d7b8944a6d1b1f348c90aee214976b34b
meeracode <71360148+meeracode@users.noreply.github.com>
2020-11-30 19:13:25 -0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
 class profile {
-  constructor(id, profession = "death", name = "John doe", numberOfyears= 0, website = "https://death.com") {
+  constructor(id, profession = "death", name = "John doe", numberOfyears= 0, website = "https://someoneuploaded.com") {
     this.id = id;
     this.name = name;
     this.profession = profession;
